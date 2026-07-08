a1e3922775f66bfbbc15f8ba36d17a933cb3b312
Sangeetha <sangeethanil79@gmail.com>
2026-07-08 11:59:05 +0530

    dataset loaded

---
+++ day1.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv(r"C:\Users\SA\Downloads\zaalima\Customer-Churn-Prediction-Lifetime-Value-LTV-Engine-Project\WA_Fn-UseC_-Telco-Customer-Churn (1).csv")
+print(df.head())
