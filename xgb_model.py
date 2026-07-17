19fb227012f57ca699ebfc58f6b3f0a52fb050bd
Sagar Kasar <you@example.com>
2026-07-17 12:25:41 +0530

    Tuning and evaluation of XGBoost model

--- xgb_model.py
+++ xgb_model.py
@@ -5,15 +5,23 @@ import shap
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import standardscaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, precision_score, recall_score, f1_score
 
 # Load the dataset
 def load_and_split_data(file_path):
     df = pd.read_csv(file_path)
     
     # Separate features and target variable
+    df['Churn'] = df['Churn'].map({'No': 0, 'Yes': 1})
+    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+    df['TotalCharges'] = df['TotalCharges'].fillna(df['TotalCharges'].median())
+
     x = df.drop(columns=['customerID', 'Churn'])
     y = df['Churn']
+
+    # 5. Convert all remaining text columns into numeric 0 and 1 flags
+    x = pd.get_dummies(x, drop_first=True)
 
     # Split the data into training and testing sets
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42, stratify=y)
@@ -34,11 +42,27 @@ def train_xgboost_model(x_train, y_train):
 
     return xgb_model
 
+
+def tuning_and_evaluation_xgb_model(x_test, y_test, xgb_model):
+    # Make predictions
+    y_pred = xgb_model.predict(x_test)
+
+    # Evaluate the model
+    print("Classification Report:")
+    print(classification_report(y_test, y_pred))
+
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    print(f"Precision: {precision:.4f}, \n Recall: {recall:.4f}, \n F1 Score: {f1:.4f}")
+
+
 if __name__ == "__main__":
-    file_path = r"G:\WA_Fn-UseC_-Telco-Customer-Churn (1).csv"
+    file_path = "WA_Fn-UseC_-Telco-Customer-Churn (1).csv"
     x_train, x_test, y_train, y_test = load_and_split_data(file_path)
     xgboost_model = train_xgboost_model(x_train, y_train)
-
+    xgb_model = tuning_and_evaluation_xgb_model(x_test, y_test, xgboost_model)
 
 
 
