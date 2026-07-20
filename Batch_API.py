3aa6bee0ed9e44b62df8a2e01bee93b160ed6cdf
Sangeetha <sangeethanil79@gmail.com>
2026-07-20 18:39:27 +0530

    Add batch prediction request schema

---
+++ Batch_API.py
@@ -0,0 +1,27 @@
+from typing import List
+from pydantic import BaseModel
+
+class CustomerData(BaseModel):
+    gender: str 
+    senior_citizen: int
+    partner: str
+    dependents: str
+    tenure: int
+    phone_service: str
+    multiple_lines: str
+    internet_service: str
+    online_security: str
+    online_backup: str
+    device_protection: str
+    tech_support: str
+    streaming_tv: str
+    streaming_movies: str
+    contract: str
+    paperless_billing: str
+    payment_method: str
+    monthly_charges: float
+    total_charges: float
+
+class BatchPredictionRequest(BaseModel):
+    customers: List[CustomerData]
+
