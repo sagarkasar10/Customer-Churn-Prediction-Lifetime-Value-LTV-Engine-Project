1f1aba60495028ba65994191d760b6117dcb34c6
Sangeetha <sangeethanil79@gmail.com>
2026-07-21 21:03:03 +0530

    Sangeetha/Implement async batch runner utility

--- Batch_API.py
+++ Batch_API.py
@@ -1,5 +1,7 @@
 from typing import List
 from pydantic import BaseModel
+import asyncio 
+from functools import partial
 
 class CustomerData(BaseModel):
     gender: str 
@@ -25,3 +27,15 @@ class CustomerData(BaseModel):
 class BatchPredictionRequest(BaseModel):
     customers: List[CustomerData]
 
+async def run_in_thread(func, *args, **kwargs):
+    """Run a synchronous function in a separate thread using asyncio."""
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(None, partial(func, *args, **kwargs))
+
+async def run_batch(customers,predict_func):
+    """Run predictions concurrently for multiple customers"""
+    tasks = [
+        run_in_thread(predict_func, customer) 
+        for customer in customers
+    ]
+    return await asyncio.gather(*tasks)
