6d0201b19a1281ad11c7c0282fca8ed8b352414e
Sagar Kasar <you@example.com>
2026-07-23 20:59:23 +0530

    Update xgb_model code and align with main

--- xgb_model.py
+++ xgb_model.py
@@ -1,35 +1,8 @@
-import pandas as pd
-import numpy as np
 import xgboost as xgb
 import shap 
 import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, precision_score, recall_score, f1_score
 
-# Load the dataset
-def load_and_split_data(file_path):
-    df = pd.read_csv(file_path)
-    
-    # Separate features and target variable
-    df['Churn'] = df['Churn'].map({'No': 0, 'Yes': 1})
-    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
-    df['TotalCharges'] = df['TotalCharges'].fillna(df['TotalCharges'].median())
-
-    x = df.drop(columns=['customerID', 'Churn'])
-    y = df['Churn']
-
-    # 5. Convert all remaining text columns into numeric 0 and 1 flags
-    x = pd.get_dummies(x, drop_first=True)
-
-    # Split the data into training and testing sets
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42, stratify=y)
-
-    print(f"Data loaded successfully!")
-    print(f"Training set shape: {x_train.shape}, Testing set shape: {x_test.shape}")
-
-    return x_train, x_test, y_train, y_test
 
 def train_xgboost_model(x_train, y_train):
     # Initialize the XGBoost classifier
@@ -38,7 +11,7 @@ def train_xgboost_model(x_train, y_train):
     # Train the model
     xgb_model.fit(x_train, y_train)
 
-    print("Model trained successfully!")
+    print("XGBoost Model trained successfully!")
 
     return xgb_model
 
@@ -71,29 +44,6 @@ def shap_summary_plot(shap_values, x_train):
     shap.summary_plot(shap_values, x_train, plot_type="bar")
     plt.show()
     #plt.savefig("shap_summary_plot.png")
-
-
-
-
-if __name__ == "__main__":
-    file_path = "WA_Fn-UseC_-Telco-Customer-Churn (1).csv"
-
-    # 1. Load data
-    x_train, x_test, y_train, y_test = load_and_split_data(file_path)
-
-    # 2. Train the model (this holds your actual trained XGBoost object)
-    xgboost_model = train_xgboost_model(x_train, y_train)
-
-    # 3. Evaluate the model 
-    tuning_and_evaluation_xgb_model(x_test, y_test, xgboost_model)
-
-    # 4. Pass the actual trained model 'xgboost_model' into SHAP
-    explainer, shap_values = initialize_shap(xgboost_model, x_train)
-
-    # 5. Generate SHAP summary plot
-    shap_summary_plot(shap_values, x_train)
-
-
 
 
 
