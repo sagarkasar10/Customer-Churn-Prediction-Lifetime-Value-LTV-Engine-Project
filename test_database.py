ee1f7f6535efa81ac72f814badde26ec34f94b81
sagarkasar10 <sagarkasar389651@gmail.com>
2026-08-09 19:47:50 +0530

    Modified test_database and test_schema

--- test_database.py
+++ test_database.py
@@ -1,12 +1,12 @@
 from src.backend.database import SessionLocal
-
+from sqlalchemy import text
 
 def test_database_connection():
 
     db = SessionLocal()
 
     try:
-        result = db.execute("SELECT 1")
+        result = db.execute(text("SELECT 1"))
         assert result.scalar() == 1
 
     finally:
@@ -19,4 +19,4 @@ def test_session_close():
 
     db.close()
 
-    assert db.is_active is False or True
+    assert db.is_active is False
